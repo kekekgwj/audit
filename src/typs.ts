e297697dedccce66627d898efe3ce0a0194cc50e
weijie <guo.weijie@outlook.com>
2023-04-04 15:59:30 +0800

    feat: add basic types

--- src/typs.ts
+++ src/typs.ts
@@ -4,9 +4,24 @@
 interface IPropertiesProps {
 	title: string;
 	data: IPropertiesData[];
-	children: React.ReactElement;
+	children?: React.ReactElement;
 }
 
 interface IPropertiesData {
 	[key: string]: string;
 }
+
+/**
+ * 响应
+ */
+
+interface IResponse {
+	data: IDTO;
+}
+interface IDTO {
+	nodes: INode[];
+	edges: IEdge[];
+}
+
+interface INode {}
+interface IEdge {}
