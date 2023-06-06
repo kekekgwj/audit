43e63d540494daf5c5d8d49c631b86459ecb6ac8
weijie <guo.weijie@outlook.com>
2023-06-06 15:44:20 +0800

    f

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig(({ mode, command }) => {
 		resolve: {
 			// https://cn.vitejs.dev/config/#resolve-alias
 			alias: {
+				src: '/src',
 				// 设置路径
 				'~': path.resolve(__dirname, '/'),
 				'@': path.resolve(__dirname, '/src/'),
